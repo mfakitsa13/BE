edc7d0dc061dafebd7264e2ee53a2537528f00d2
mfakitsa13 <131189164+mfakitsa13@users.noreply.github.com>
2025-04-16 14:42:00 +0300

    Update DrivingSchoolRepository.java

--- drivingschoolsystem/src/main/java/drivingschoolsystem/repositories/DrivingSchoolRepository.java
+++ drivingschoolsystem/src/main/java/drivingschoolsystem/repositories/DrivingSchoolRepository.java
@@ -4,7 +4,6 @@ import drivingschoolsystem.entities.DrivingSchool;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface DrivingSchoolRepository extends JpaRepository<DrivingSchool, Integer> {
